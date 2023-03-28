82a80b80d4cf766ae809366849f31ee4724133b5
wwdfdo <wwdfdo@gmail.com>
2023-03-28 16:40:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="bg-blue-400 w-1/12 h-[80vh]"></div>
+      <div className="bg-red-500 w-2/12 h-[80vh]"></div>
+      <div className="bg-yellow-500 w-7/12 h-[80vh]"></div>
+      <div className="bg-green-500 w-2/12 h-[80vh]"></div>
+    </div>
+  );
+}
+
+export default App;
